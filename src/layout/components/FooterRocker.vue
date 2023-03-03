86084f726b0551f274af106ed5f1c10156d5508a
Bảo Nguyên <tn2842003@gmail.com>
2023-03-03 12:00:00 +0700

    QLNH

---
+++ src/layout/components/FooterRocker.vue
@@ -0,0 +1,13 @@
+<template>
+<footer class="page-footer">
+    <p class="mb-0"> Nhà hàng Nguyên Trần</p>
+</footer>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
